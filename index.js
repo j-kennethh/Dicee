d53f70f0123f23011d8fabee121ec46703608926
John Kenneth <johnkennethagpaoa@gmail.com>
2023-04-16 01:06:14 +0800

    refactored js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelector(".img1").setAttribute("src", randomImageSource1);
+document.querySelector(".img2").setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
